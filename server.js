f36f138d77c55c827a33ec029f9b17973fab4ec0
Clara <claramagaldi@gmail.com>
2022-03-07 18:47:57 -0500

    Trying to deploy

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path")
+
 require("dotenv").config({ path: "./config.env" });
 
 //const port = process.env.PORT || 5000;
@@ -12,8 +14,13 @@ if (port == null || port == "") {
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
+app.use(express.static(path.join(__dirname, "client", "build")))
 // get driver connection
 const dbo = require("./db/conn");
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
   // perform a database connection when server starts
